63060c091b23fe108f7aef7b3660f0e3333f9c88
Catherine Tucker <ImCatherineNoel@gmail.com>
2020-07-31 20:29:26 -0700

    have some data rendering via props

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,42 +1,43 @@
 //Importing dependencies
-import React, { useEffect, useState } from "react";
-
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 //Importing project files
 import './App.css';
-
+import Character from './components/Character.js'
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
-  const [data, setData] =useState([]);
+	// Try to think through what state you'll need for this app before starting. Then build out
+	// the state properties here.
+	const [data, setData] = useState([]);
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
-  useEffect(() => {
-    axios
-      .get(`https://rickandmortyapi.com/api/character/`)
-      .then(response => {
-        const data = response.data.results;
-        //testing
-            console.log(data);
-
+	// Fetch characters from the API in an effect hook. Remember, anytime you have a
+	// side effect in a component, you want to think about which state and/or props it should
+	// sync up with, if any.
+	useEffect(() => {
+		axios.get(`https://rickandmortyapi.com/api/character/`)
+			.then((response) => {
+				const data = response.data.results;
         setData(data);
-      })
-      .catch(error => {
-        console.log('Error:', error);
-      });
-      }, []);
+        //testing
+				console.log(data);				
+			})
+			.catch((error) => {
+				console.log('Error:', error);
+			});
+	}, []);
 
-  return (
-    <div className='App'>
-      <h1 className='Header'>Characters</h1>
-      {/* testing information display*/}
-      <div>
-      <p>{data.id}</p>
-      </div>
-    </div>
-  );
-}
+	return (
+		<div className='App'>
+			<h1 className='Header'>Characters</h1>
+			{data.map((character) => {
+				return (
+					<p>	
+            <Character name={character.name} imgUrl={character.image}/>
+					</p>
+				);
+			})}
+		</div>
+	);
+};
 
 export default App;
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,16 @@
-// Write your Character component here
+//Importing dependencies
+import React from 'react';
+import styled from 'styled-components';
+
+
+const Character = props => {
+     return (
+          <div>
+          <h1>{props.name}</h1>
+          <img alt={props.name} src={props.imgUrl}/>
+          <p>Bio:</p>
+     </div>
+     );
+}
+
+export default Character;
